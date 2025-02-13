81446a6374846a5b27320a6ca6b191504008fa81
tier3guy <gavinash6290@gmail.com>
2025-02-13 09:55:24 +0530

    fix: server actions/apis

--- components/forms/join-waitlist-form.tsx
+++ components/forms/join-waitlist-form.tsx
@@ -24,9 +24,7 @@ export default function JoinWaitlistForm() {
                 },
                 body: JSON.stringify({ name, email })
             });
-            console.log(response);
             const data = await response.json();
-            console.log(data);
             setResp(data);
         } catch (error) {
             console.error(error);
